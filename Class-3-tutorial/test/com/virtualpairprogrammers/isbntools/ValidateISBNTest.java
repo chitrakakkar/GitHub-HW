19a7a55dbe0e3b5f414bcd9f5ddcf5db0ab3240d
chitrakakkar <chitrakakkar@yahoo.co.in>
2021-02-25 23:24:09 -0500

    Second TDD commit.

--- Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -9,8 +9,20 @@ public class ValidateISBNTest {
 	@Test
 	public void checkAValidISBN() {
 		ValidatorISBN validator = new ValidatorISBN();
-		boolean result = validator.checkISBN(140449116);
-		assertTrue(result);
+		boolean result = validator.checkISBN("0140449116");
+		assertTrue("first ISBN", result);
+		
+		result = validator.checkISBN("0140177396");
+		assertTrue("second ISBN", result);
+		
+	}
+	
+	@Test
+	public void checkAnInvalidISBN() {
+		
+		ValidatorISBN validator = new ValidatorISBN();
+		boolean result = validator.checkISBN("0140449117");
+		assertFalse(result);
 		
 	}
 }
