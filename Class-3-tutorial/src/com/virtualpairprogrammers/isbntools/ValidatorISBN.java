bb65bef7c5302fd9baa0dcbedfc35bed64a7e7f9
chitrakakkar <chitrakakkar@yahoo.co.in>
2021-02-25 23:56:15 -0500

    Final commit of TDD tutorial

--- Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ValidatorISBN.java
+++ Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ValidatorISBN.java
@@ -4,10 +4,20 @@ public class ValidatorISBN {
 
 	public boolean checkISBN(String isbn) 
 	{
+		if(isbn.length() != 10)
+		{
+			throw new NumberFormatException("ISBN numbers must be 10 digits long");
+		}
+		
 		int total=0;
 		
 		for (int i=0; i<10; i++) 
 		{
+			if(!Character.isDigit(isbn.charAt(i)))
+			{
+				throw new NumberFormatException("ISBN numbers can only contain numeric digits");
+				
+			}
 			total += isbn.charAt(i)*(10-i);
 		}
 		
@@ -15,6 +25,7 @@ public class ValidatorISBN {
 		{
 			return true;
 		}
+		
 		else 
 		{
 			return false;
--- Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ValidateISBNTest {
-
+public class ValidateISBNTest 
+{
 	@Test
 	public void checkAValidISBN() {
 		ValidatorISBN validator = new ValidatorISBN();
@@ -13,16 +13,26 @@ public class ValidateISBNTest {
 		assertTrue("first ISBN", result);
 		
 		result = validator.checkISBN("0140177396");
-		assertTrue("second ISBN", result);
-		
-	}
-	
+		assertTrue("second ISBN", result);		
+	}	
 	@Test
 	public void checkAnInvalidISBN() {
 		
 		ValidatorISBN validator = new ValidatorISBN();
 		boolean result = validator.checkISBN("0140449117");
-		assertFalse(result);
-		
+		assertFalse(result);		
+	}
+	
+	@Test(expected = NumberFormatException.class)
+	public void nineDigitISBNsAreNotAllowed() 
+	{
+		ValidatorISBN validator = new ValidatorISBN();
+		validator.checkISBN("123456789");		
+	}
+	@Test(expected = NumberFormatException.class)
+	public void nonNumericUSBNsAreNotAllowed()
+	{
+		ValidatorISBN validator = new ValidatorISBN();
+		validator.checkISBN("helloworld");	
 	}
 }
